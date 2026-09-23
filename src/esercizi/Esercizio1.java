f8c990011a90a5a53212f9ae473da9c867883251
marco.valcanover <marco.valcanover@LAB04-22.curie.local>
2026-09-23 09:08:02 +0200

    aggiunto la visualizzazione a video "ciao"

---
+++ src/esercizi/Esercizio1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package esercizi;
+
+/**
+ *
+ * @author marco.valcanover
+ */
+public class Esercizio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("prova");
+        System.out.println("ciao");
+    }
+    
+}
